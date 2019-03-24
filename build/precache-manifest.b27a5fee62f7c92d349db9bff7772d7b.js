e24d737882523658aa28d51effa4789cab159da2
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-03-23 22:26:38 -0300

    feat: Add static build to gh-pages

---
+++ build/precache-manifest.b27a5fee62f7c92d349db9bff7772d7b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    revision: '229c360febb4351a89df',
+    url: '/static/js/runtime~main.229c360f.js'
+  },
+  {
+    revision: '77b2193afede9adcb15c',
+    url: '/static/js/main.77b2193a.chunk.js'
+  },
+  {
+    revision: '9612e858872788b446ab',
+    url: '/static/js/1.9612e858.chunk.js'
+  },
+  {
+    revision: '969a5a1e6f7b0873979fb711bd02ada1',
+    url: '/index.html'
+  }
+]
