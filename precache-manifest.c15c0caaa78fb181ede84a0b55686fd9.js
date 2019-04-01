7acbe896382ec7c6c676072e972c3d491de7471b
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-04-01 10:22:53 +0100

    Updates

---
+++ precache-manifest.c15c0caaa78fb181ede84a0b55686fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "291b1b2f4fa99a75a31a",
+    "url": "/static/js/main.291b1b2f.chunk.js"
+  },
+  {
+    "revision": "44a8a74531e41d6e1f29",
+    "url": "/static/js/1.44a8a745.chunk.js"
+  },
+  {
+    "revision": "44a8a74531e41d6e1f29",
+    "url": "/static/css/1.c6590aa1.chunk.css"
+  },
+  {
+    "revision": "b0829cc51c8b0b224931c659a04bbc57",
+    "url": "/index.html"
+  }
+];
